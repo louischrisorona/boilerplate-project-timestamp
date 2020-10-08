abac247885f75c25f12e3dab7287a6c6ffa121ab
Chris <louischrisorona@gmail.com>
2020-10-08 03:58:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,6 +24,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/", function (req, res) {
+  res.json({
+    unix: Date.now(),
+    utc: Date()
+    })
+})
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  let dateString = new Date(req.params.date_string)
+  if (/\d.{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString)
+
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() })
+  }
+
+  let dateObject = new Date(dateString)
+  
+  if(dateObject.toString() === 'Invalid Date') {
+    res.json({ error: "Invalid Date" })
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() })
+  }
+})
 
 
 // listen for requests :)
